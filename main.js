51c4abcab28576736cb70712a18efdb86c6a9741
VanessaVeizaga <vanessaveizaga@gmail.com>
2022-10-25 15:27:03 -0300

    Se agregan imágenes a página Beauty & Relax

--- main.js
+++ main.js
@@ -9,22 +9,48 @@ function colorTextoBotones(texto) {
     }
 }
 
-
 function masajesCorporales() {
     colorTextoBotones("Masajes corporales")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>Masajes corporales</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/masajesCorporales.jpg" alt="Mujer recibiendo masajes corporales">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML = `<h5>Masajes corporales</h5>
+    descripcion.innerHTML = `
     <ul>
-        <li>Masajes relajantes</li>
-        <li>Masajes descontracturantes</li>
-        <li>Masajes Holisticos</li>
+        <li class="subtitulo">Masajes relajantes</li>
+            <p>Es un masaje enfocado en las zonas corporales donde generalmente se acumula tensión 
+            muscular como lo es la espalda y cuello, además de buscar el estado de relajación y salud 
+            mental en la persona que se le es aplicado.</p>
+        <li class="subtitulo">Masajes descontracturantes</li>
+            <p>Un masaje descontracturante es un tipo de masaje que tiene como objetivo liberar los 
+            músculos y las articulaciones para ayudarlos a funcionar en su nivel óptimo. El propósito 
+            del masaje es relajar y aflojar los músculos rígidos y tensos.</p>
+        <li class="subtitulo">Masajes Holisticos</li>
+            <p>El masaje holístico es una técnica que consiste en tratar el cuerpo y la mente como un todo. 
+            La palabra ‘holos’ proviene del griego y se puede traducir como ‘todo’. conecta todos los aspectos
+             que componen al ser humano: cuerpo, mente, alma y espíritu. Todos se contemplan como un todo, 
+             buscando alcanzar un equilibrio natural en zonas estratégicas en las que habitualmente se 
+             concentra la tensión, como el cuello, la espalda, la cabeza y los brazos.</p>
     </ul>`    
 }
 
 function masajesFaciales() {
     colorTextoBotones("Masajes faciales")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>Masajes faciales</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/masajesFaciales.png" alt="Mujer recibiendo masajes faciales">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML = `<h5>Masajes faciales</h5>
+    descripcion.innerHTML = `
     <p>El masaje facial es un tratamiento de belleza que abarca un conjunto de técnicas 
     basadas en manipulaciones manuales sobre el cutis. Por lo general, se realizan pequeños 
     movimientos sobre el rostro, aplicando diferentes grados de intensidad, superficial o profunda</p>`
@@ -32,25 +58,49 @@ function masajesFaciales() {
 
 function unasEsculpidas() {
     colorTextoBotones("Uñas esculpidas")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>Uñas esculpidas</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/unasEsculpidas.jpg" alt="Uñas esculpidas">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML = `<h5>Uñas esculpidas</h5>
-    <p>Técnica donde  lo que se hace es “esculpir” una uña artificial sobre la uña natural, 
+    descripcion.innerHTML = `
+    <p>Técnica donde lo que se hace es “esculpir” una uña artificial sobre la uña natural, 
     fijándola con luz ultravioleta. Luego se puede esmaltar sobre dicha uña con esmalte común o 
     semipermanente y aplicar diferentes tipos de diseños y/o decoraciones.</p>`
 }
 
 function bellezaDePies() {
     colorTextoBotones("Belleza de pies")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>Belleza de pies</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/bellezaDePies.jpg" alt="Uñas esculpidas">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML =`<h5>Belleza de pies</h5>
-    <p>consiste en el arreglo de las uñas, donde se cortan, se liman y se dan forma a la uñas. 
+    descripcion.innerHTML =`
+    <p>Consiste en el arreglo de las uñas, donde se cortan, se liman y se dan forma a la uñas. 
     También se empujan, se limpian y se cortan las cutículas. Para finalizar se realiza el esmaltado en gel.</p>`
 }
 
 function bellezaDeManos() {
     colorTextoBotones("Belleza de manos")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>Belleza de manos</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/bellezaDeManos.jpg" alt="Uñas esculpidas">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML = `<h5>Belleza de manos</h5>
+    descripcion.innerHTML = `
     <p>Un tratamiento completo para el cuidado y belleza de las manos, con el que se consigue lucir una uña arreglada, 
     desde el limado, el arrastre de la cutícula y el maquillado. También supone una hidratación profunda de las manos 
     que permite recuperar su frescura y suavidad.</p>`
@@ -58,16 +108,32 @@ function bellezaDeManos() {
 
 function maquillajeSocial() {
     colorTextoBotones("Maquillaje social")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>Maquillaje social</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/maquillajeSocial.jpg" alt="Uñas esculpidas">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML = `<h5>Maquillaje social</h5>
+    descripcion.innerHTML = `
     <p>Para el maquillaje social destacás los rasgos de la cara y tapás imperfecciones, ya que se trata de dejar a
      la persona más bella de lo que es. Se usa para 15 años, novias, madrinas</p>`
 }
 
 function platinumCard() {
     colorTextoBotones("PLATINUM CARD")
+    let portada = document.getElementById("portada")
+    if (portada != null) {
+        portada.remove()
+    }
+    let titulo = document.getElementById("titulo")
+    titulo.innerHTML = "<h3>PLATINUM CARD</h3>"
+    let imagen = document.getElementById("imagen")
+    imagen.innerHTML = '<img src="../media/Beauty/platinumCard.jpg" alt="Uñas esculpidas">'
     let descripcion = document.getElementById("descripcion")
-    descripcion.innerHTML = `<h5>PLATINUM CARD</h5>
+    descripcion.innerHTML = `
     <p>Esta tarjeta comprende los siguientes tratamientos</p>
     <ul>
         <li>4 sesiones de masajes a elección</li>
